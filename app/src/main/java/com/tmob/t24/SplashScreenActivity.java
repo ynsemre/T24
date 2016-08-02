36d7b39e315f244e953eba078deaab40364fde74
Yunus Emre FIRAT <yemre.firat@gmail.com>
2016-08-03 00:29:33 +0300

    @SplashScreenActivity is created.

---
+++ app/src/main/java/com/tmob/t24/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+package com.tmob.t24;
+
+import android.os.Bundle;
+
+import com.tmob.t24.utils.BaseActivity;
+
+public class SplashScreenActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
